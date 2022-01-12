a7fad5463404c8916dff049959623f839eb53442
Ekaterina Semenova <semenova@mpia.de>
2022-01-12 13:14:10 +0100

    moved files

--- source/test_interpolation.py
+++ source/test_interpolation.py
@@ -44,17 +44,6 @@ if __name__ == '__main__':
     excluded = np.full(M, False)
     excluded[ind] = True
 
-    " Copy the grid of models omitting a random point"
-    all_short = {}
-    for k in all_parameters:
-        all_short.update({ k : all_parameters[k][~excluded] })
-
-    " Create array of input parameters for interpolation "
-    interpol_parameters = { 'teff':None, 'logg':None, 'feh':None, 'vturb':None }
-    for k in interpol_parameters:
-        interpol_parameters[k] = all_parameters[k][excluded]
-
-    interp_f, pars_to_interpolate, models_mask = NDinterpolate(interpol_parameters, all_short)
 
     "Create directory to save interpolated models"
     today = datetime.date.today().strftime("%b-%d-%Y")
@@ -64,8 +53,19 @@ if __name__ == '__main__':
     comparison = {}
     struct_keys = ['tau500', 'temp', 'ne', 'vturb']
     for i in np.where(excluded)[0]:
-        # mask = np.full(M, False)
-        # mask[i] = True
+        mask = np.full(M, False)
+        mask[i] = True
+        " Copy the grid of models omitting a random point"
+        all_short = {}
+        for k in all_parameters:
+            all_short.update({ k : all_parameters[k][~mask] })
+
+        " Create array of input parameters for interpolation "
+        interpol_parameters = { 'teff':None, 'logg':None, 'feh':None, 'vturb':None }
+        for k in interpol_parameters:
+            interpol_parameters[k] = all_parameters[k][mask]
+
+        interp_f, pars_to_interpolate, models_mask = NDinterpolate(interpol_parameters, all_short)
 
 
         name_exc = all_parameters['file'][i].replace('.mod', '')
@@ -79,9 +79,13 @@ if __name__ == '__main__':
         # it returns array of interpolated models, so for one take 0th element
         interpolated_structure = interp_f(point)[0]
 
+        # bring Ne back to linear scale
+        value[2] = 10**value[2]
+        interpolated_structure[2] = 10**(interpolated_structure[2])
+        
         # write to file
         ma_interpol = model_atmosphere()
-        ma_interpol.id = name_exc
+        ma_interpol.id = f"{name_exc}_interpol"
         ma_interpol.header = f"This model has been interpolated using routines from E.Magg. \n* Date: {today}"
 
         ma_interpol.depth_scale_type = 'TAU500'
@@ -93,16 +97,16 @@ if __name__ == '__main__':
         ma_interpol.ndep = len(ma_interpol.depth_scale)
 
         ma_interpol.temp = interpolated_structure[1]
-        ma_interpol.ne = interpolated_structure[2]
+        ma_interpol.ne = 10**(interpolated_structure[2])
         ma_interpol.vmac = np.zeros(len(ma_interpol.depth_scale))
         ma_interpol.vturb = interpolated_structure[3]
 
-        ma_interpol.write( f"{save_interpol_dir}/atmos.{name_exc}_interpol", format='m1d')
+        ma_interpol.write( f"{save_interpol_dir}/atmos.{ma_interpol.id}", format='ts')
 
 
         # write original model to file
         ma_orig = model_atmosphere()
-        ma_orig.id = name_exc
+        ma_orig.id = f"{name_exc}_original"
         ma_orig.header = f" "
 
         ma_orig.depth_scale_type = 'TAU500'
@@ -114,11 +118,11 @@ if __name__ == '__main__':
         ma_orig.ndep = len(ma_interpol.depth_scale)
 
         ma_orig.temp = value[1]
-        ma_orig.ne = value[2]
+        ma_orig.ne = 10**(value[2])
         ma_orig.vmac = np.zeros(len(ma_interpol.depth_scale))
         ma_orig.vturb = value[3]
 
-        ma_orig.write( f"{save_interpol_dir}/atmos.{name_exc}_original", format='m1d')
+        ma_orig.write( f"{save_interpol_dir}/atmos.{ma_orig.id}", format='ts')
 
 
         comparison.update( { name_exc : {} } )
