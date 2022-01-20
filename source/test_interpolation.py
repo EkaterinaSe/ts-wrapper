293cfc6712aa0b3ddc932afef1875d1603ff7051
Ekaterina Semenova <semenova@mpia.de>
2022-01-20 10:57:36 +0100

    end of individual tests

--- source/test_interpolation.py
+++ source/test_interpolation.py
@@ -34,6 +34,7 @@ if __name__ == '__main__':
     "Read all model atmospheres"
     all_parameters = get_all_ma_parameters(atmos_path, \
                                             format = 'marcs', debug=True)
+
     # size of the model grid
     M = len(all_parameters['file'])
     # how many points to remove?
@@ -41,18 +42,17 @@ if __name__ == '__main__':
 
     "Generate random indexes to exclude models from the grid"
     ind = ( np.random.uniform(low=0.0, high=M-1.0, size=count) ).astype(int)
+
     excluded = np.full(M, False)
     excluded[ind] = True
 
 
-    "Create directory to save interpolated models"
-    today = datetime.date.today().strftime("%b-%d-%Y")
-    save_interpol_dir = f"./interpolated_models_{today}"
-    mkdir(save_interpol_dir)
-
     comparison = {}
     struct_keys = ['tau500', 'temp', 'ne', 'vturb']
+
+    count_done = 0
     for i in np.where(excluded)[0]:
+        print(f"{count_done+1} / {count}")
         mask = np.full(M, False)
         mask[i] = True
         " Copy the grid of models omitting a random point"
@@ -61,76 +61,29 @@ if __name__ == '__main__':
             all_short.update({ k : all_parameters[k][~mask] })
 
         " Create array of input parameters for interpolation "
-        interpol_parameters = { 'teff':None, 'logg':None, 'feh':None, 'vturb':None }
+        interpol_parameters = { 'teff':None, 'logg':None, 'feh':None, 'vturb':None}
         for k in interpol_parameters:
             interpol_parameters[k] = all_parameters[k][mask]
 
         interp_f, pars_to_interpolate, models_mask = NDinterpolate(interpol_parameters, all_short)
 
-
         name_exc = all_parameters['file'][i].replace('.mod', '')
-        # poitns are normalised to the max value of parameter
+        # points are normalised to the max value of parameter
         # (stored in pars_to_interpolate)
         point = np.array([ all_parameters[k][i] / pars_to_interpolate[k] for k in pars_to_interpolate])
-
         # ma.depth_scale, ma.temp, ma.ne, ma.vturb
         value = all_parameters['structure'][i]
 
         # it returns array of interpolated models, so for one take 0th element
         interpolated_structure = interp_f(point)[0]
 
-        # bring Ne back to linear scale
-        value[2] = 10**value[2]
-        interpolated_structure[2] = 10**(interpolated_structure[2])
-        
-        # write to file
-        ma_interpol = model_atmosphere()
-        ma_interpol.id = f"{name_exc}_interpol"
-        ma_interpol.header = f"This model has been interpolated using routines from E.Magg. \n* Date: {today}"
-
-        ma_interpol.depth_scale_type = 'TAU500'
-        ma_interpol.logg = all_parameters['logg'][i]
-        ma_interpol.teff = all_parameters['teff'][i]
-        ma_interpol.feh = all_parameters['feh'][i]
-
-        ma_interpol.depth_scale = interpolated_structure[0]
-        ma_interpol.ndep = len(ma_interpol.depth_scale)
-
-        ma_interpol.temp = interpolated_structure[1]
-        ma_interpol.ne = 10**(interpolated_structure[2])
-        ma_interpol.vmac = np.zeros(len(ma_interpol.depth_scale))
-        ma_interpol.vturb = interpolated_structure[3]
-
-        ma_interpol.write( f"{save_interpol_dir}/atmos.{ma_interpol.id}", format='ts')
-
-
-        # write original model to file
-        ma_orig = model_atmosphere()
-        ma_orig.id = f"{name_exc}_original"
-        ma_orig.header = f" "
-
-        ma_orig.depth_scale_type = 'TAU500'
-        ma_orig.logg = all_parameters['logg'][i]
-        ma_orig.teff = all_parameters['teff'][i]
-        ma_orig.feh = all_parameters['feh'][i]
-
-        ma_orig.depth_scale = value[0]
-        ma_orig.ndep = len(ma_interpol.depth_scale)
-
-        ma_orig.temp = value[1]
-        ma_orig.ne = 10**(value[2])
-        ma_orig.vmac = np.zeros(len(ma_interpol.depth_scale))
-        ma_orig.vturb = value[3]
-
-        ma_orig.write( f"{save_interpol_dir}/atmos.{ma_orig.id}", format='ts')
-
-
         comparison.update( { name_exc : {} } )
         for j in range(len(struct_keys)):
             comparison[name_exc].update( { struct_keys[j] : {'interpol' : interpolated_structure[j], 'orig' : value[j] } } )
+            # print(f"{struct_keys[j]} diff: {max(np.abs( interpolated_structure[j] - value[j] ))}")
         for k in ['teff', 'logg', 'feh', 'vturb']:
             comparison[name_exc].update( { k :  all_parameters[k][i] } )
-
+        count_done+=1
     with open('./interpolation_MC_test.pkl', 'wb') as f:
         pickle.dump(comparison, f)
 
