db4ff080b3559e8650b828be5e8cb0ea82f4ddce
Ekaterina Semenova <semenova@mpia.de>
2022-02-01 16:34:39 +0100

    elemental abundances to be supplied with fundamental parameters

--- source/configure_setup.py
+++ source/configure_setup.py
@@ -12,47 +12,9 @@ def read_elemental_setup(file):
     elements = {}
 
     data = np.genfromtxt(file, \
-    dtype=[('element', 's'), ('abFlag', 'i4'), \
-            ('abundStart', 'f4'), ('abundEnd', 'f4'), ('abundStep', 'f4'), \
+    dtype=[('element', 's'),
             ('nlteFlag', 'i4'), ('modelAtom', 's'), \
             ('nlteBinGrid', 's'), ('nlteAuxFile', 's') ])
-
-    # read what abundances are requested
-    for i in range(len(data['element']))
-        el = data['element'][i]
-        elements.update( el : { 'abund' : None, 'nlte':False } )
-
-        abFlag = data['abFlag'][i]
-        if abFlag == 0: # solar
-        # TODO: not sure how to implement this without hard-coding solar abundances
-            print(f"solar abundances not supported, change for {el} in {file}")
-            exit()
-        elif abFlag == 1: # use new abundance
-            abundNew = data['abundStart'][i]
-            elements[el]['abund'] = [abundNew]
-        elif abFlag == 2: # use an array of abundances
-            abStart = data['abStart'][i]
-            abEnd = data['abEnd'][i]
-            abStep = data['abStep'][i]
-
-            abundArray = np.arange( abStart, abEnd, abStep )
-            if abEnd not in abundArray:
-                abundArray = np.hstack( [ abundArray, [abEnd] ] )
-            elements[el]['abund'] = abundArray
-
-    # check that requested number of abundances is compatible among elements
-    abundCount = 1
-    for el in elements:
-        if len(elements[el]['abund']) != abundCount:
-            abundCount = len(elements[el]['abund'])
-            for otherEl in elements:
-                if otherEl != el:
-                    if len(elements[otherEl]['abund']) == 1:
-                        elements[otherEl]['abund'] = np.full(abundCount, elements[otherEl]['abund'][0])
-                    else:
-                        print(f"Requested {abundCount} abundances for {el}, \
-which does not match with other elements. Change {file}.")
-                        exit()
 
 
     # read which files to use in case of NLTE
@@ -65,6 +27,9 @@ which does not match with other elements. Change {file}.")
     return elements
 
 
+def read_random_input_parameters(file):
+    params = {}
+    return params
 
 class setup(object):
     def __init__(self, cnfg_file='./config.txt'):
