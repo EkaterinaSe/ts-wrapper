a1bdb11f8cd0ed0b5ae25285f147cf7d5492c2fa
Ekaterina Semenova <semenova@mpia.de>
2022-02-10 09:52:59 +0100

    start re-scaling the NLTE grid

--- source/configure_setup.py
+++ source/configure_setup.py
@@ -133,11 +133,8 @@ To set up NLTE, use 'nlte_config' flag\n {50*'*'}")
         "Model atmosphere grid"
         if self.debug:
             print("preparing model atmosphere interpolator...")
-        modelAtmGrid= get_all_ma_parameters(self.atmos_path, \
+        modelAtmGrid, atmDepthScale = get_all_ma_parameters(self.atmos_path, \
                                         format = self.atmos_format, debug=self.debug)
-        depth_scale = [ [d] for d in modelAtmGrid['depth_scale'] ]
-        depth_scale = np.array(depth_scale)
-        print(np.shape(depth_scale), np.shape(np.unique(depth_scale)))
         interpFunction, normalisedCoord = NDinterpolate_MA(modelAtmGrid, interpolCoords )
 
 
@@ -151,7 +148,7 @@ To set up NLTE, use 'nlte_config' flag\n {50*'*'}")
                     print(f"preparing interpolator for {el}")
 
                 nlteData = read_full_grid( self.inputParams['elements'][el]['nlteGrid'], \
-                                            self.inputParams['elements'][el]['nlteAux'] )
+                                            self.inputParams['elements'][el]['nlteAux'], rescale=True, depthScale = atmDepthScale )
                 # interpolate over abundance?
                 interpolCoords_el = interpolCoords.copy()
                 if min(nlteData['abund']) == max(nlteData['abund']):
